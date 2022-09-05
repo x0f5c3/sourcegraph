096d6e86be1c3307c0c09f53a04f09f575969303
Philipp Spiess <hello@philippspiess.com>
2022-09-05 11:13:56 +0200

    Move injection of new APIs into the no-op controller

--- client/shared/src/api/client/connection.ts
+++ client/shared/src/api/client/connection.ts
@@ -1,17 +1,12 @@
-import { TextDocumentPositionParameters } from '@sourcegraph/client-api'
-import { MaybeLoadingResult } from '@sourcegraph/codeintellify'
 import * as comlink from 'comlink'
-import { from, Observable, of, Subscription } from 'rxjs'
-import { first, map } from 'rxjs/operators'
+import { from, Subscription } from 'rxjs'
+import { first } from 'rxjs/operators'
 import { Unsubscribable } from 'sourcegraph'
-import { newCodeIntelAPI } from '../../codeintel/api'
-import { CodeIntelContext } from '../../codeintel/legacy-extensions/api'
 
 import { PlatformContext, ClosableEndpointPair } from '../../platform/context'
 import { isSettingsValid } from '../../settings/settings'
 import { FlatExtensionHostAPI, MainThreadAPI } from '../contract'
 import { ExtensionHostAPIFactory } from '../extension/api/api'
-import { proxySubscribable } from '../extension/api/common'
 import { InitData } from '../extension/extensionHost'
 import { registerComlinkTransferHandlers } from '../util'
 
@@ -101,80 +96,8 @@ export async function createExtensionHostClientConnection(
     return {
         subscription,
         // api: proxy, the old code
-        api: injectNewCodeintel(proxy, {
-            requestGraphQL: platformContext.requestGraphQL,
-            telemetryService: platformContext.telemetryService,
-            settings: platformContext.settings,
-            // TODO searchContext: ???
-        }),
+        api: proxy,
         mainThreadAPI: newAPI,
         exposedToClient,
     }
-}
-
-// Replaces codeintel functions from the "old" extension/webworker extension API
-// with new implementations of code that lives in this repository. The old
-// implementation invoked codeintel functions via webworkers, and the codeintel
-// implementation lived in a separate repository
-// https://github.com/sourcegraph/code-intel-extensions Ideally, we should
-// update all the usages of `comlink.Remote<FlatExtensionHostAPI>` with the new
-// `CodeIntelAPI` interfaces, but that would require refactoring a lot of files.
-// To minimize the risk of breaking changes caused by the deprecation of
-// extensions, we monkey patch the old implementation with new implementations.
-// The benefit of monkey patching is that we can optionally disable if for
-// customers that choose to enable the legacy extensions.
-function injectNewCodeintel(
-    old: comlink.Remote<FlatExtensionHostAPI>,
-    context: CodeIntelContext
-): comlink.Remote<FlatExtensionHostAPI> {
-    const codeintel = newCodeIntelAPI(context)
-    function thenMaybeLoadingResult<T>(promise: Observable<T>): Observable<MaybeLoadingResult<T>> {
-        return promise.pipe(
-            map(result => {
-                const maybeLoadingResult: MaybeLoadingResult<T> = { isLoading: false, result }
-                return maybeLoadingResult
-            })
-        )
-    }
-
-    const codeintelOverrides: Pick<
-        FlatExtensionHostAPI,
-        | 'getHover'
-        | 'getDocumentHighlights'
-        | 'getReferences'
-        | 'getDefinition'
-        | 'getLocations'
-        | 'hasReferenceProvidersForDocument'
-    > = {
-        hasReferenceProvidersForDocument(textParameters) {
-            return proxySubscribable(codeintel.hasReferenceProvidersForDocument(textParameters))
-        },
-        getLocations(id, parameters) {
-            console.log({ id })
-            return proxySubscribable(thenMaybeLoadingResult(codeintel.getImplementations(parameters)))
-        },
-        getDefinition(parameters) {
-            return proxySubscribable(thenMaybeLoadingResult(codeintel.getDefinition(parameters)))
-        },
-        getReferences(parameters, context) {
-            console.log({ parameters })
-            return proxySubscribable(thenMaybeLoadingResult(codeintel.getReferences(parameters, context)))
-        },
-        getDocumentHighlights: (textParameters: TextDocumentPositionParameters) => {
-            return proxySubscribable(codeintel.getDocumentHighlights(textParameters))
-        },
-        getHover: (textParameters: TextDocumentPositionParameters) => {
-            return proxySubscribable(thenMaybeLoadingResult(codeintel.getHover(textParameters)))
-        },
-    }
-
-    return new Proxy(old, {
-        get(target, prop) {
-            const codeintelFunction = (codeintelOverrides as any)[prop]
-            if (codeintelFunction) {
-                return codeintelFunction
-            }
-            return Reflect.get(target, prop, ...arguments)
-        },
-    })
 }
--- client/shared/src/extensions/createNoopLoadedController.ts.ts
+++ client/shared/src/extensions/createNoopLoadedController.ts.ts
@@ -1,8 +1,17 @@
-import { NEVER } from 'rxjs'
+import * as comlink from 'comlink'
+import { NEVER, Observable } from 'rxjs'
+import { map } from 'rxjs/operators'
 
+import { TextDocumentPositionParameters } from '@sourcegraph/client-api'
+import { MaybeLoadingResult } from '@sourcegraph/codeintellify'
+
+import { FlatExtensionHostAPI } from '../api/contract'
+import { proxySubscribable } from '../api/extension/api/common'
 import { createExtensionHostAPI } from '../api/extension/extensionHostApi'
 import { createExtensionHostState } from '../api/extension/extensionHostState'
 import { pretendRemote } from '../api/util'
+import { newCodeIntelAPI } from '../codeintel/api'
+import { CodeIntelContext } from '../codeintel/legacy-extensions/api'
 import { PlatformContext } from '../platform/context'
 import { isSettingsValid } from '../settings/settings'
 
@@ -30,7 +39,12 @@ export function createNoopController(platformContext: PlatformContext): Controll
                     null,
                     null
                 )
-                const extensionHostAPI = pretendRemote(createExtensionHostAPI(extensionHostState))
+                const extensionHostAPI = injectNewCodeintel(pretendRemote(createExtensionHostAPI(extensionHostState)), {
+                    requestGraphQL: platformContext.requestGraphQL,
+                    telemetryService: platformContext.telemetryService,
+                    settings: platformContext.settings,
+                    // TODO searchContext: ???
+                })
 
                 resolve(extensionHostAPI)
             })
@@ -38,4 +52,69 @@ export function createNoopController(platformContext: PlatformContext): Controll
 
         unsubscribe: () => {},
     }
+}
+
+// Replaces codeintel functions from the "old" extension/webworker extension API
+// with new implementations of code that lives in this repository. The old
+// implementation invoked codeintel functions via webworkers, and the codeintel
+// implementation lived in a separate repository
+// https://github.com/sourcegraph/code-intel-extensions Ideally, we should
+// update all the usages of `comlink.Remote<FlatExtensionHostAPI>` with the new
+// `CodeIntelAPI` interfaces, but that would require refactoring a lot of files.
+// To minimize the risk of breaking changes caused by the deprecation of
+// extensions, we monkey patch the old implementation with new implementations.
+// The benefit of monkey patching is that we can optionally disable if for
+// customers that choose to enable the legacy extensions.
+export function injectNewCodeintel(
+    old: comlink.Remote<FlatExtensionHostAPI>,
+    context: CodeIntelContext
+): comlink.Remote<FlatExtensionHostAPI> {
+    const codeintel = newCodeIntelAPI(context)
+    function thenMaybeLoadingResult<T>(promise: Observable<T>): Observable<MaybeLoadingResult<T>> {
+        return promise.pipe(
+            map(result => {
+                const maybeLoadingResult: MaybeLoadingResult<T> = { isLoading: false, result }
+                return maybeLoadingResult
+            })
+        )
+    }
+
+    const codeintelOverrides: Pick<
+        FlatExtensionHostAPI,
+        | 'getHover'
+        | 'getDocumentHighlights'
+        | 'getReferences'
+        | 'getDefinition'
+        | 'getLocations'
+        | 'hasReferenceProvidersForDocument'
+    > = {
+        hasReferenceProvidersForDocument(textParameters) {
+            return proxySubscribable(codeintel.hasReferenceProvidersForDocument(textParameters))
+        },
+        getLocations(id, parameters) {
+            console.log({ id })
+            return proxySubscribable(thenMaybeLoadingResult(codeintel.getImplementations(parameters)))
+        },
+        getDefinition(parameters) {
+            return proxySubscribable(thenMaybeLoadingResult(codeintel.getDefinition(parameters)))
+        },
+        getReferences(parameters, context) {
+            console.log({ parameters })
+            return proxySubscribable(thenMaybeLoadingResult(codeintel.getReferences(parameters, context)))
+        },
+        getDocumentHighlights: (textParameters: TextDocumentPositionParameters) =>
+            proxySubscribable(codeintel.getDocumentHighlights(textParameters)),
+        getHover: (textParameters: TextDocumentPositionParameters) =>
+            proxySubscribable(thenMaybeLoadingResult(codeintel.getHover(textParameters))),
+    }
+
+    return new Proxy(old, {
+        get(target, prop) {
+            const codeintelFunction = (codeintelOverrides as any)[prop]
+            if (codeintelFunction) {
+                return codeintelFunction
+            }
+            return Reflect.get(target, prop, ...arguments)
+        },
+    })
 }
